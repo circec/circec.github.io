3b1bdbee7991ef8200d7a39edf0967de8a2d83ca
circec <circe_1975@hotmail.com>
2020-10-10 16:19:25 -0400

    update

--- lesson40/js/hamburger.js
+++ lesson40/js/hamburger.js
@@ -1,15 +1,8 @@
-function toggleMenu() {
-    document.getElementById("navigation").classList.toggle("hide");
-  }
-  
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
 
-
-
-//const hambutton = document.querySelector('.ham');
-//const mainnav = document.querySelector('.navigation')//
-
-//hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
-//window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
