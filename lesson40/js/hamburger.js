5f169b71da83de3a2a9a989c345c236121473bc7
circec <circe_1975@hotmail.com>
2020-10-10 18:29:12 -0400

    update

--- lesson40/js/hamburger.js
+++ lesson40/js/hamburger.js
@@ -1,3 +1,5 @@
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
   }
+  const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('#navigation')
