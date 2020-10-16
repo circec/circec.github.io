e77ed2cc6ead9ffd347f70f1dd7de1beb5ea9796
circec <circe_1975@hotmail.com>
2020-10-16 15:34:52 -0400

    update

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -3,6 +3,12 @@ let todaydate = date.getDate();
 
 document.getElementById("update").innerHTML = date.toDateString();
 
+function maini(){
+    doBanner();
+    getDate();
+    getSummary();
+}
+
 function doBanner(data){
     if (new Date().getDay() == "5" || data== true){
         document.getElementById('banner').classList.toggle('hidden');
