26336fe2fa98799a4ec5dfb41ef21132d4dab791
circec <circe_1975@hotmail.com>
2020-10-28 14:00:54 -0400

    update

--- lesson7/js/imageload.js
+++ lesson7/js/imageload.js
@@ -1,4 +1,4 @@
-const imagesToLoad - document.querySelectorAll("[data-src]");
+const imagesToLoad = document.querySelectorAll("[data-src]");
 
 
 const imgOptions = {
@@ -6,15 +6,22 @@ const imgOptions = {
     rootMargin: "0px 0px 50px 0px"
 };
 
-const loadimages - (image) => {
+const loadimages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload - () => [image.removeAttribute('data-src');];
+    image.onload = () => [image.removeAttribute('data-src');];
 };
 
 if('IntersectionObserver' in window) {
-    const imgObserver - new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {---
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
 
         });
     }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+    else {
+    
 }
