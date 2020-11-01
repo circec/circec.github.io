018737236848b49d987ff85aec67e6686ed9a7fa
circec <circe_1975@hotmail.com>
2020-11-01 11:11:45 -0400

    update

--- lesson6/js/date.js
+++ lesson6/js/date.js
@@ -23,6 +23,7 @@ const daynames = [
 
 /* Summary */
 
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,14 @@
-function windChill(tempF, speed) {
+/*function getWindchill() {
+  const temperature = parseInt(document.getElementById('currTemp').innerText)
+  const windSpeed = parseInt(document.getElementById('windSpeed').innerText)
+  const windChill = 35.74 + (0.625*temperature) - (35.75*Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16))
+  document.getElementById(windChill).innerText = windChill.toFixed(2)*/
+
+  /*getWindchill()*/
+
+  function windChill(tempF, speed) {
     let s = Math.pow(speed, 0.16);
     f = 35.74 + (0.6215 * tempF) - (35.75 * s) + (0.4275 * tempF * s);
-    return f.toFixed(2);
+    return f.toFixed(2) + "&#8457;";
   }
+  
