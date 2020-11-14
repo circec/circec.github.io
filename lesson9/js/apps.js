788ef6abb21f8703f0444fafb98461aab03adb83
circec <circe_1975@hotmail.com>
2020-11-14 19:03:44 -0400

    update

--- lesson9/js/apps.js
+++ lesson9/js/apps.js
@@ -10,7 +10,7 @@ fetch(requestURL)
     // console.table(jsonObject); // temporary checking for valid response and data parsing
   
 
-  const towns = jsonObject['towns'];
+  /*const towns = jsonObject['towns'];
 
   towns.forEach(towns => {
 
@@ -49,4 +49,36 @@ fetch(requestURL)
         document.querySelector('div.cards').appendChild(card);
     }
 });
-});
+});*/
+
+const towns = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
+                let towninfodenise = document.createElement('section');
+                let textcontainer = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let motto = document.createElement('h3');
+                let founded = document.createElement('p');
+                let population = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+
+                //dle code for populating info
+                towninfodenise.setAttribute('class', towns[i].name);
+                h2.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                founded.textContent = "Year Founded: " + towns[i].yearFounded;
+                population.textContent = "Population: " + towns[i].currentPopulation;
+                rain.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+                image.setAttribute('src', "images/" + towns[i].photo);
+                image.setAttribute('alt', towns[i].name);
+
+                textcontainer.append(h2, motto, founded, population, rain);
+                towninfodenise.append(textcontainer, image);
+
+                document.querySelector('div.towns').appendChild(towninfodenise);
+            }
+
+        }
+    });
