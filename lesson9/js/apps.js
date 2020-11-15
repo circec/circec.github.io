1956dedaf3947ac6800201dc9a8bff2769ae6ec9
circec <circe_1975@hotmail.com>
2020-11-14 22:46:46 -0400

    update

--- lesson9/js/apps.js
+++ lesson9/js/apps.js
@@ -10,48 +10,6 @@ fetch(requestURL)
     // console.table(jsonObject); // temporary checking for valid response and data parsing
   
 
- /* const towns = jsonObject['towns'];
-
-  towns.forEach(towns => {
-
-
-    if (towns.name == 'Preston' || towns.name == 'Fish Haven' || towns.name == 'Soda Springs') {
-
-        let card = document.createElement('section');
-
-        let display = document.createElement('div');
-        let motto = document.createElement('h3');
-        let yearFounded = document.createElement('h3');
-        let currentPopulation = document.createElement('h3');
-        let photo = document.createElement('img');
-        let name = document.createElement('h2');
-        let averageRainfall = document.createElement('h3');
-
-        photo.setAttribute('src', towns.photo);
-        motto.innerHTML = `<i>${towns.motto}`;
-        display.setAttribute('class', 'display');
-        card.setAttribute('class', 'sectdiv');
-
-        currentPopulation.innerHTML = `Population: ${towns.currentPopulation}`;
-        yearFounded.innerHTML = `Year Founded: ${towns.yearFounded}`;
-        name.innerHTML = `${towns.name}`;
-        averageRainfall.innerHTML = `Average Rainfall :${towns.averageRainfall}`;
-        card.appendChild(display);
-        display.appendChild(name);
-
-        display.appendChild(motto);
-        display.appendChild(yearFounded);
-        display.appendChild(currentPopulation);
-        display.appendChild(averageRainfall);
-
-        card.appendChild(photo);
-
-        document.querySelector('div.towns').appendChild(card);
-    }
-});
-});*/
-
-
 const towns = jsonObject['towns'];
 
         for (let i = 0; i < towns.length; i++) {
