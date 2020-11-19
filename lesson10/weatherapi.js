665e31c322f71bdc112ccbd3231f015b8844b942
circec <circe_1975@hotmail.com>
2020-11-19 14:21:06 -0400

    update

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -1,10 +1,20 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f6541e29cb635a8e5272a1705e4acb7c';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=f6541e29cb635a8e5272a1705e4acb7c';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
        console.log(jsObject);
+
+       const curtemp = document.querySelector('#current-temp');
+       const iconsource = document.querySelector('#imagesrc');
+       const weathericon = document.querySelector('#icon');
+
+       curtemp.innerHTML = jsObject.main.temp;
+       iconsource.textContent = 'https://api.openweathermap.org/img/w/${jsObject.weather[0].icon}.png';
+       weathericon.setAttribute('src', isrc);
+       weathericon.setAttribute('alt', jsObject.weather[0].description);
+       
   
-       document.getElementById('current-temp').textContent = jsObject.main.temp;
+      /*document.getElementById('current-temp').textContent = jsObject.main.temp;
 
   
 const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
@@ -12,6 +22,6 @@ const desc = jsObject.weather[0].description;  // note how we reference the weat
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
-document.getElementById('current-temp').textContent = jsObject.main.temp;
+document.getElementById('current-temp').textContent = jsObject.main.temp;*/
 
 });
