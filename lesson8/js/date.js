fbd0026296a437030652e44812b8ad4eaca98518
circec <circe_1975@hotmail.com>
2020-11-20 17:55:55 -0400

    update

--- lesson8/js/date.js
+++ lesson8/js/date.js
@@ -5,7 +5,7 @@ document.getElementById("update").innerHTML = date.toDateString();
 
 //pancakes
 
-if (new Date().getDay() == 5) {
+/*if (new Date().getDay() == 5) {
     document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!</br>9:00 a.m. Saturday at the city park pavilion.";
 } else {
     document.getElementById("pancakes").style.display = "none";
@@ -18,7 +18,19 @@ const daynames = [
     "Thursday",
     "Friday",
     "Saturday"
-];
+];*/
+(function fridayBanner() {
+    let d = new Date();
+    const ban = document.querySelector('.panke');
+  
+    if (d.getDay() == 5)
+     {
+      ban.style.display = 'block';
+    } else {
+      ban.style.display = 'none';
+    };
+  }());
+
 
 // Range Adjustment
 function adjustRating(rating) {
